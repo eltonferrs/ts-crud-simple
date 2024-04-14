638ae4e327c7c8f5bada498c7d1f7690f355628d
eltonferreira9728 <eltonferreira9728@gmail.com>
2024-04-14 12:41:48 -0300

    feat:Crud methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Inicializando!!")
+const nameList: string[] = [];
+
+const postName = (name:string) => {
+    nameList.push(name);
+}
+
+const getNames = () => {
+    nameList.forEach(name => console.log(name));
+}
+
+const removeName = (name:string) => {
+    const indexname = nameList.findIndex(value => value == name);
+    if(indexname >= 0) nameList.splice(indexname, 1)
+}
+
+const  updateName = (name:string, newName: string) => {
+    const indexname = nameList.findIndex(value => value == name);
+    if(indexname >= 0) nameList[indexname] = newName;
+}
